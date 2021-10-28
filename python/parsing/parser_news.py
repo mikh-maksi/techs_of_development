d597e13c55f013e3d56a9867be31fe52766122ef
mikh-maksi <newlevelcenter@gmail.com>
2021-10-28 12:36:24 +0300

    parser

--- python/parsing/parser_news.py
+++ python/parsing/parser_news.py
@@ -14,9 +14,7 @@ def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('li')
     print(len(items))
-    # print(items)
     cars = []
-    # el = items.find('a', class_='name')
     n= 0
     for item in items:
         n=n+1
@@ -26,19 +24,7 @@ def get_content(html):
             print(elem_out)
         else:
             elem_out = ''
-        # print(elem_out)
-    #     uah_price = item.find('span', class_='size15')
-    #     if uah_price:
-    #         uah_price = uah_price.get_text().replace(' • ', '')
-    #     else:
-    #         uah_price = 'Цену уточняйте'
-    #     cars.append({
-    #         'title': item.find('div', class_='na-card-name').get_text(strip=True),
-    #         'link': HOST + item.find('span', class_='link').get('href'),
-    #         'usd_price': item.find('strong', class_='green').get_text(),
-    #         'uah_price': uah_price,
-    #         'city': item.find('svg', class_='svg_i16_pin').find_next('span').get_text(),
-    #     })
+
     print(n)
     return cars
 
